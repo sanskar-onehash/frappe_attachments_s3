748aa49839c60babab4d97cf33bceb2f35d75769
Sanskar Soni <sanskarsoni300@gmail.com>
2025-04-04 01:45:30 +0530

    fix: prepared report file get_content fix

--- frappe_s3_attachment/frappe_s3_attachment/overrides/file.py
+++ frappe_s3_attachment/frappe_s3_attachment/overrides/file.py
@@ -1,4 +1,5 @@
 import re
+import requests
 import urllib.parse
 
 import frappe
@@ -6,6 +7,8 @@ from frappe import _
 
 from frappe.utils import call_hook_method, cint, get_files_path, get_hook_method, get_url
 from frappe.core.doctype.file.file import File
+from frappe.core.doctype.file.utils import decode_file_content
+from frappe_s3_attachment.controller import extract_key_and_file_name
 
 frappe.utils.logger.set_log_level("DEBUG")
 logger = frappe.logger("api", allow_site=True, file_count=50)
@@ -14,10 +17,10 @@ class FileOverride(File):
     def set_is_private(self):
         if self.file_url:
              self.is_private = cint(
-                self.file_url.startswith("/private") or 
+                self.file_url.startswith("/private") or
                 "frappe_s3_attachment.controller.generate_file" in self.file_url
             )
-             
+
     def set_file_name(self):
         if not self.file_name and not self.file_url:
             frappe.throw(
@@ -35,7 +38,54 @@ class FileOverride(File):
             else:
                 self.file_name = self.file_url.split("/")[-1]
                 parts = self.file_name.split("_")
-                if len(parts) > 1:  
-                    self.file_name = "_".join(parts[1:]) 
+                if len(parts) > 1:
+                    self.file_name = "_".join(parts[1:])
         else:
             self.file_name = re.sub(r"/", "", self.file_name)
+
+    def get_content(self):
+        if self.is_folder:
+            frappe.throw(_("Cannot get file contents of a Folder"))
+
+        if self.file_url and self.file_url.startswith(("http://", "https://")):
+            self.validate_file_url()
+            file_path = self.file_url
+
+            if "frappe_s3_attachment.controller.generate_file" in file_path:
+                site_base_url = frappe.utils.get_url()
+                key, file_name = extract_key_and_file_name(file_path)
+                signed_url_request = f"{site_base_url}/api/method/frappe_s3_attachment.controller.generate_signed_url?key={key}&file_name={file_name}"
+                response = requests.get(signed_url_request)
+
+                if response.status_code == 200:
+                    file_path = response.json().get("message")
+                else:
+                    frappe.throw(f"Failed to generate signed URL: {response.status_code}")
+
+            response = requests.get(file_path)
+            if response.status_code == 200:
+                self._content = response.content
+            else:
+                frappe.throw(f"Failed to get file from {file_path} (Status: {response.status_code})")
+        else:
+            if self.get("content"):
+                self._content = self.content
+                if self.decode:
+                    self._content = decode_file_content(self._content)
+                    self.decode = False
+                # self.content = None # TODO: This needs to happen; make it happen somehow
+                return self._content
+
+            file_path = self.get_full_path()
+            # read the file
+            with open(file_path, mode="rb") as f:
+                self._content = f.read()
+
+        try:
+            # for plain text files
+            self._content = self._content.decode()
+        except UnicodeDecodeError:
+            # for .png, .jpg, etc
+            pass
+
+        return self._content
