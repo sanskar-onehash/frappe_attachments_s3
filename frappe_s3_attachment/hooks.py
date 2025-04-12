d65eb112946e94b4fc401ae4d2df49155dd99bad
Sanskar Soni <sanskarsoni300@gmail.com>
2025-04-12 09:00:52 +0530

    feat: move files between public and private on visibility toggle

--- frappe_s3_attachment/hooks.py
+++ frappe_s3_attachment/hooks.py
@@ -99,6 +99,7 @@ override_doctype_class = {
 
 doc_events = {
     "File": {
+        "before_save": "frappe_s3_attachment.controller.transfer_file",
         "after_insert": "frappe_s3_attachment.controller.file_upload_to_s3",
         "on_trash": "frappe_s3_attachment.controller.delete_from_cloud"
     }
@@ -136,4 +137,3 @@ doc_events = {
 # override_whitelisted_methods = {
 # 	"frappe.desk.doctype.event.event.get_events": "frappe_s3_attachment.event.get_events"
 # }
-
